f2c73796fc6c6b6babe8bf8b2b2906deb8bc6660
A1EXAGUI1AR16 <alex.aguilar5480@gmail.com>
2025-09-18 20:08:25 -0600

    correccion de codigo para enviar datos de la bd a el login principal

--- modulos/login.js
+++ modulos/login.js
@@ -1,10 +1,21 @@
+import {paginaPrincipal} from "./principal.js"
 export function login(interfazLogin){
     interfazLogin.innerHTML = `
     <h2>Login</h2>
-    <input id="email" type="text" placeholder="Email"><br>
+    <input id="username" type="text" placeholder="Nombre de Usuario"><br>
     <input id="password" type="password" placeholder="Contraseña"><br>
     <button id="loginBtn">Entrar</button>
   `;
-
-  
+document.getElementById('loginBtn').onclick =
+    async ()  =>{
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+        const res = await fetch(`http://localhost:3000/users?username${username}&password=${password}`);
+        const user = await res.json();
+        if(user.length > 0) {
+            paginaPrincipal(interfaz);
+        }   else    {
+            alert("Usuario o Contraseña Incorrectos");
+        }
+    }  
 }
